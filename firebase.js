ae157eff32baf21a0f20e8163ca85a4d0a742803
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-09-13 15:25:27 -0400

    added the fix for firebase.auth()

--- firebase.js
+++ firebase.js
@@ -9,6 +9,27 @@ document.addEventListener("DOMContentLoaded", () => {
     messagingSenderId: "501853322857",
     appId: "1:501853322857:web:05c9418e7a50eb7523acf8"
   };
+  console.log(firebaseConfig);
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  let txtEmail = document.getElementById("txtEmail");
+  let txtPassword = document.getElementById("txtPassword");
+  let btnSignUp = document.getElementById("btnSignUp");
+  let btnLogin = document.getElementById("btnLogin");
+  let btnLogout = document.getElementById("btnLogout");
+  console.log(btnLogin);
+
+  btnLogin.addEventListener("click", e => {
+    //get email and password
+    let email = txtEmail.value;
+    let password = txtPassword.value;
+    debugger;
+    const auth = firebase.auth();
+    //sign in
+    console.log(email);
+    const promise = auth.signInWithEmailAndPassword(email, password);
+    promise.catch(e => console.log(e.message));
+  });
 });
